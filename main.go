82f3f749d1755c7eb092ecd91024b1cb911b91c0
BUHUIQUMING-D <2511052698@qq.com>
2020-12-10 21:39:15 +0800

    函数

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("hello world")
+func main() {
+	fmt.Println(Add(1, 3, 4, 5)) // output 13
+}
+
+func Add(a ...int) (result int) {
+	for _, v := range a {
+		result += v //   result = result + v
+	}
+	return result
 }
